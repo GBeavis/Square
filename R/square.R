3548ba20d7a2314f0e88ad224a882b7ee6dbad3b
GBeavis <40696620+GBeavis@users.noreply.github.com>
2018-06-29 19:37:00 +0100

    Add files via upload

---
+++ R/square.R
@@ -0,0 +1,30 @@
+#' @title Square
+#'
+#' @description Squares a number
+#'
+#' @param x a value to get squared
+#'
+#' @return The square of the input
+#'
+#' @examples square(2)
+#'
+#' @export square
+square<-function(x){
+  return(x^2)
+}
+
+#' @title Cube
+#'
+#' @description Cubes a number
+#'
+#' @param x a value to get cubed
+#'
+#' @return The cube of the input
+#'
+#' @examples cube(2)
+#'
+#' @export cube
+cube<-function(x){
+  return(x^3)
+}
+
